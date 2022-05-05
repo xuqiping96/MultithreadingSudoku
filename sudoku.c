222567e289ddf491fe9d1e9ec1dc80bacdcf5324
Qiping Xu <xqppp111@gmail.com>
2022-05-05 15:02:38 +0800

    实现判断功能

--- sudoku.c
+++ sudoku.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 #define MAX_LINE_LEN 256
@@ -21,6 +22,7 @@ typedef struct
 
 typedef struct
 {
+  pthread_t tid;
   int psize;
   int **grid;
   bool valid;
@@ -30,13 +32,19 @@ typedef struct
 pthread_t row_tid;
 pthread_t column_tid;
 SubGridStruct *sub_grid_struct;
-GridStruct grid_struct_row;
-GridStruct grid_struct_column;
+GridStruct *grid_struct_row;
+GridStruct *grid_struct_column;
 
 //获取SubGrid的值
+//psize为子网格的大小
 int getSubGridValue(int psize, int **grid, int subGrid, int subX, int subY)
 {
-  return 0;
+  int value;
+  int sub_startX = psize * ((subGrid - 1) / psize) + 1;
+  int sub_startY = psize * ((subGrid - 1) % psize) + 1;
+  value = grid[sub_startX + subX - 1][sub_startY + subY - 1];
+
+  return value;
 }
 
 //打印SubGrid索引值
@@ -50,28 +58,62 @@ void printSubGrid(int psize, int **grid)
 }
 
 // subGrid是否有效
-//对sub_grid_struct[subGrid]进行判断，psize为sub_grid_struct[subGrid].psize，grid为sub_grid_struct[subGrid].grid
-bool subGridValid(int psize, int **grid, int subGrid)
+//传入sub_grid_struct数组第i个子网格的地址
+bool subGridValid(SubGridStruct * sub_grid_struct)
 {
   bool flag = true;
+  int sub_psize = sub_grid_struct->psize;
+  bool sub_grid_flags[sub_psize * sub_psize + 1];
+  for(int i = 1; i <= sub_psize * sub_psize; i++) {
+    sub_grid_flags[i] = false;
+  }
+  //判断过程
+  for(int row = 1; row <= sub_psize; row++) {
+    for(int column = 1; column <= sub_psize; column++) {
+      if(sub_grid_flags[sub_grid_struct->grid[row][column]] == false) {
+        sub_grid_flags[sub_grid_struct->grid[row][column]] = true;
+      } else {
+        flag = false;
+        return flag;
+      }
+    }
+  }
+  
+
   return flag;
 }
 
 // subGrid是否已经完成
-bool subGridComplete(int psize, int **grid, int subGrid)
+bool subGridComplete(SubGridStruct * sub_grid_struct)
 {
   bool flag = true;
+  int zero_count = 0;
+  bool sub_grid_flags[sub_grid_struct->psize];
+  for(int i = 1; i <= sub_grid_struct->psize; i++) {
+    sub_grid_flags[i] = false;
+  }
+  int sub_psize = sub_grid_struct->psize;
+  for(int row = 1; row <= sub_psize; row++) {
+    for(int column = 1; column <= sub_psize; column++) {
+      if(sub_grid_struct->grid[row][column] == 0) {
+        flag = false;
+        return flag;
+      }
+    }
+  }
+
   return flag;
 }
 
 // subGrid线程执行的方法
+// 传入指向sub_grid_struct数组元素i的指针
 void *subGridHelper(void *arg)
 {
   //判断子网格是否完成，若完成则判断是否有效
   SubGridStruct *sub_grid_struct = (SubGridStruct *)arg;
-  if(subGridComplete(sub_grid_struct->psize, sub_grid_struct->grid, sub_grid_struct->id) == false) {
+  if(subGridComplete(sub_grid_struct) == false) {
     sub_grid_struct->complete = false;
-  } else if(subGridValid(sub_grid_struct->psize, sub_grid_struct->grid, sub_grid_struct->id) == false) {
+  } else if(subGridValid(sub_grid_struct) == false) {
     sub_grid_struct->valid = false;
   }
 
@@ -84,7 +126,7 @@ bool rowValid(int psize, int **grid)
   bool flag = true;
   bool row_flags[psize + 1][psize + 1];
   for(int row = 0; row < psize + 1; row++) {
-    for(int column = 0; column < psize + 1; column++) {
+    for(int column = 1; column < psize + 1; column++) {
       row_flags[row][column] = false;
     }
   }
@@ -126,6 +168,7 @@ void *rowHelper(void *arg) {
   //判断行是否完成，若完成则判断是否有效；
   if(rowComplete(grid_struct->psize, grid_struct->grid) == false) {
     grid_struct->complete = false;
+  } else {
     if(rowValid(grid_struct->psize, grid_struct->grid) == false) {
       grid_struct->valid = false;
     }
@@ -138,6 +181,23 @@ void *rowHelper(void *arg) {
 bool columnValid(int psize, int **grid)
 {
   bool flag = true;
+  bool column_flags[psize + 1][psize + 1];
+  for(int row = 1; row <= psize; row++) {
+    for(int column = 1; column <= psize; column++) {
+      column_flags[row][column] = false;
+    }
+  }
+
+  for(int column = 1; column <= psize; column++) {
+    for(int row = 1; row <= psize; row++) {
+      if(column_flags[grid[row][column]][column] == false) {
+        column_flags[grid[row][column]][column] = true;
+      } else {
+        flag = false;
+        return flag;
+      }
+    }
+  }
 
   return flag;
 }
@@ -146,6 +206,15 @@ bool columnValid(int psize, int **grid)
 bool columnComplete(int psize, int **grid)
 {
   bool flag = true;
+
+  for(int column = 1; column <= psize; column++) {
+    for(int row = 1; row <= psize; column++) {
+      if(grid[row][column] == 0) {
+        flag = false;
+        return flag;
+      }
+    }
+  }
   return flag;
 }
 
@@ -153,6 +222,15 @@ bool columnComplete(int psize, int **grid)
 void *columnHelper(void *arg)
 {
   //判断列是否完成，若完成则判断是否有效；
+  GridStruct *grid_struct = (GridStruct *)arg;
+  //判断行是否完成，若完成则判断是否有效；
+  if(columnComplete(grid_struct->psize, grid_struct->grid) == false) {
+    grid_struct->complete = false;
+  } else {
+    if(columnValid(grid_struct->psize, grid_struct->grid) == false) {
+      grid_struct->valid = false;
+    }
+  }
 
   return NULL;
 }
@@ -166,24 +244,50 @@ void *helper(void *arg)
 //执行任务，这里初始化线程等
 void task(int psize, int **grid, SubGridStruct *subGridStruct)
 {
+  int err;
   //初始化行和列的GridStruct *grid_struct
-  grid_struct_row.complete = true;
-  grid_struct_row.valid = true;
-  grid_struct_row.psize = psize;
-  grid_struct_row.grid = grid;
-  grid_struct_column.complete = true;
-  grid_struct_column.valid = true;
-  grid_struct_column.psize = psize;
-  grid_struct_column.grid = grid;
+  grid_struct_row = (GridStruct *)calloc(1, sizeof(GridStruct));
+  grid_struct_column = (GridStruct *)calloc(1, sizeof(GridStruct));
+
+  grid_struct_row->psize = psize;
+  grid_struct_row->grid = grid;
+  grid_struct_row->complete = true;
+  grid_struct_row->valid = true;
+
+  grid_struct_column->psize = psize;
+  grid_struct_column->grid = grid;
+  grid_struct_column->complete = true;
+  grid_struct_column->valid = true;
   //创建行的线程和列的线程
-  
+  err = pthread_create(&grid_struct_row->tid, NULL, rowHelper, grid_struct_row);
+  if(err != 0) {
+    fprintf(stderr, "pthread_create error.\n");
+  }
+  err = pthread_create(&grid_struct_column->tid, NULL, rowHelper, grid_struct_row);
+  if(err != 0) {
+    fprintf(stderr, "pthread_create error.\n");
+  }
   //循环创建子网格的线程
+  for(int i = 1; i <= psize; i++) {
+    err = pthread_create(&subGridStruct[i].tid, NULL, subGridHelper, &subGridStruct[i]);
+    if(err != 0) {
+      fprintf(stderr, "pthread_create error.\n");
+    }
+  }
 }
 
 //设置结果
 //将此函数用在checkSudoku中，线程执行之后
 void setResult(int psize, bool *complete, bool *valid, SubGridStruct *subGridStruct)
 {
+  if(grid_struct_row->complete == false || grid_struct_column->complete == false) {
+    *complete = false;
+  }
+
+  if(grid_struct_row->valid == false || grid_struct_column ->valid == false) {
+    *valid = false;
+  }
+
   for(int i = 1; i <= psize; i++) {
     if(subGridStruct[i].complete == false) {
       *complete = false;
@@ -197,6 +301,7 @@ void setResult(int psize, bool *complete, bool *valid, SubGridStruct *subGridStr
 //检查
 void checkSudoku(int psize, int **grid, bool *complete, bool *valid)
 { 
+  int err;
   //动态分配子网格结构，使用getSubGridValue初始化各个子网格
   sub_grid_struct = (SubGridStruct *)calloc(psize + 1, sizeof(SubGridStruct));
   for(int i = 1; i < psize + 1; i++) {
@@ -217,8 +322,26 @@ void checkSudoku(int psize, int **grid, bool *complete, bool *valid)
   //使用task,创建行、列、子网格的线程 
   task(psize, grid, sub_grid_struct);
 
+  err = pthread_join(grid_struct_row->tid, NULL);
+  if(err != 0) {
+    fprintf(stderr, "pthread_join error.\n");
+  }
+  err = pthread_join(grid_struct_column->tid, NULL);
+  if(err != 0) {
+    fprintf(stderr, "pthread_join error.\n");
+  }
+  for(int i = 1; i <= psize; i++) {
+    err = pthread_join(sub_grid_struct[i].tid, NULL);
+    if(err != 0) {
+      fprintf(stderr, "pthread_join error.\n");
+    }
+  }
   //线程执行后设置验证的结果
+  setResult(psize, complete, valid, sub_grid_struct);
 
+  free(grid_struct_row);
+  free(grid_struct_column);
+  free(sub_grid_struct);
 }
 
 //读取数独数据
@@ -226,14 +349,16 @@ int readSudoku(char *filename, int ***grid)
 {
   int psize;
   FILE *fp_in;
-  char *read_line;
+  char read_line[MAX_LINE_LEN];
+  char *pch;
 
   if((fp_in = fopen(filename, "r")) == NULL) {
     fprintf(stderr, "Failed to open %s.\n", filename);
     exit(EXIT_FAILURE);
   }
   fgets(read_line, MAX_LINE_LEN, fp_in);
-  psize = atoi(strtok(read_line, "\n"));
+  pch = strtok(read_line, "\n");
+  psize = atoi(pch);
 
   *grid = (int **)calloc(psize + 1, sizeof(int *));
   for(int i = 0; i < psize + 1; i++) {
@@ -242,9 +367,11 @@ int readSudoku(char *filename, int ***grid)
 
   for(int i = 1; i <= psize; i++) {
     fgets(read_line, MAX_LINE_LEN, fp_in);
-    (*grid)[i][1] = atoi(strtok(read_line, " \n"));
+    pch = strtok(read_line, " \n");
+    (*grid)[i][1] = atoi(pch);
     for(int j = 2; j <= psize; j++) {
-      (*grid)[i][j] = atoi(strtok(NULL, " \n"));
+      pch = strtok(NULL, " \n");
+      (*grid)[i][j] = atoi(pch);
     }
   }
 
